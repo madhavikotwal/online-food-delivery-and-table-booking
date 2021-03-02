4ffbb9863143525627074730aa11b3b71b6ce1fa
madhavikotwal <73238985+madhavikotwal@users.noreply.github.com>
2021-03-02 22:56:03 +0530

    Add files via upload

---
+++ public/client.js
@@ -0,0 +1,10 @@
+console.log("Client Side Script is getting executed....");
+var onButtonClick=function(){
+
+    console.log("Button is clicked");
+    var person={
+        firstname:'Reena',
+        lastname:"Nene"
+    };
+    console.log(person);
+};
